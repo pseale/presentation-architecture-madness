8c5f41477dfca1f72ebd5e43f04c8af7cff0cafb
Peter Seale <peter@pseale.com>
2015-02-06 20:20:36 -0600

    change build to vanilla VS template for easier consumption

--- MonogameDemoGame/Properties/AssemblyInfo.cs
+++ MonogameDemoGame/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MonogameDemoGame")]
-[assembly: AssemblyProduct("MonogameDemoGame")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MonogameDemoGame")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("73497c31-451c-44cb-9e33-859f2485911c")]
+[assembly: Guid("78dc32da-327d-4c87-b243-efbe50078baa")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.0.1695")]
-[assembly: AssemblyFileVersion("3.3.0.1695")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

fd8c52d122ae22d3e37ed633a46111c6ceea2433
Peter Seale <peter@pseale.com>
2015-02-07 21:22:53 -0600

    move class

--- MonogameDemoGame/Core/CameraHelper.cs
+++ MonogameDemoGame/Core/CameraHelper.cs
@@ -1,7 +1,7 @@
 using Microsoft.Xna.Framework;
 using MonogameDemoGame.Core.Domain;
 
-namespace MonogameDemoGame.Helpers
+namespace MonogameDemoGame.Core
 {
     public static class CameraHelper
     {
